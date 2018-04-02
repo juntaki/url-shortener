ede683655a218fd6e4111204b87d8d04f02ffd97
juntaki <me@juntaki.com>
2018-04-03 00:37:31 +0900

    GAE

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
-package main
+package urlshortener
 
 import (
+	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/mjibson/goon"
+	"google.golang.org/appengine/datastore"
 )
 
-func main() {
+var letters = []rune("23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
@@ -16,20 +23,60 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Get("/", handler)
-	r.Get("/admin", adminHandler)
+	r.Get("/", adminHandler)
+	r.Get("/{ShortURLID}", handler)
 
-	http.ListenAndServe(":8080", r)
+	http.Handle("/", r)
+}
+
+type ShortURL struct {
+	ID  string `datastore:"-" goon:"id"`
+	URL string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hoge"))
-	w.WriteHeader(http.StatusOK)
-	return
+	g := goon.NewGoon(r)
+
+	id := chi.URLParam(r, "ShortURLID")
+	su := &ShortURL{ID: id}
+	if err := g.Get(su); err != nil {
+		w.Write([]byte("404"))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, su.URL, http.StatusFound)
+}
+
+func randomID(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
 
 func adminHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hoge"))
+	g := goon.NewGoon(r)
+
+	id := randomID(3)
+	for {
+		su := &ShortURL{ID: id}
+		if err := g.Get(su); err != nil {
+			if err == datastore.ErrNoSuchEntity {
+				break
+			}
+		}
+		id = randomID(3)
+	}
+	url := r.URL.Query().Get("url")
+	su := &ShortURL{ID: id, URL: url}
+	_, err := g.Put(su)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("http://l.juntaki.com/" + id))
 	w.WriteHeader(http.StatusOK)
 	return
 }
